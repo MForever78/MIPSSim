68ee47133bac2142f9bf1ce621c07405cc8003c7
MForever78 <shunjian1128@gmail.com>
2015-09-23 14:12:56 +0800

    Hello World

---
+++ src/main/java/MIPSSim.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * MIPSSim
+ *
+ * A MIPS simulator
+ * Created by MForever78 on 15/9/22.
+ */
+
+public class MIPSSim {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
